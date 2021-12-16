dae09bb97b6cd29a60cf98f14991397d69bf3701
1jgrant <grantja92@gmail.com>
2021-12-16 23:17:55 +0000

    solves day4/1

---
+++ src/day4.py
@@ -0,0 +1,43 @@
+import re
+from typing import List
+from file_read_fn import input_text_to_array_ignore_empty_lines
+
+
+input_text = input_text_to_array_ignore_empty_lines('src/inputs/day4_input.txt')
+num_order_str = input_text.pop(0)
+draw_numbers = num_order_str.split(',')
+test = ['22 13 17 11  0', ' 8  2 23  4 24', '21  9 14 16  7', '6 10  3 18  5', '1 12 20 15 19']
+bingo_nums = [re.findall('[0-9]+', item) for item in input_text]
+
+
+def chunks(items: List[str], chunk_length: int):
+    return (items[i:i+chunk_length] for i in range(0, len(items), chunk_length))
+
+
+cards = list(chunks(bingo_nums, 5))
+for card in cards:
+    columns = []
+    for i in range(0, 5):
+        column = []
+        for j in range(0, 5):
+            column.append(card[j][i])
+        columns.append(column)
+    card.extend(columns)
+
+
+def get_card_and_drawn_nums(bingo_cards, draw_numbers):
+    drawn_nums = draw_numbers[:4]
+    for num in draw_numbers[4:]:
+        drawn_nums.append(num)
+        for card in bingo_cards:
+            for dimension in card:
+                bingo = all(elem in drawn_nums for elem in dimension)
+                if bingo:
+                    return (card, drawn_nums)
+
+
+winning_card, drawn_numbers = get_card_and_drawn_nums(cards, draw_numbers)
+winning_card_numbers = list(set([item for dimension in winning_card for item in dimension]))
+unmarked_numbers = [int(num) for num in winning_card_numbers if num not in drawn_numbers]
+sum_of_unmarked = sum(unmarked_numbers)
+print(sum_of_unmarked * int(drawn_numbers[-1]))
--- src/file_read_fn.py
+++ src/file_read_fn.py
@@ -1,5 +1,12 @@
 def input_text_to_array(filepath: str):
     with open(filepath, 'r', encoding='utf-8') as text:
         lines = text.read().splitlines()
-        array_of_values = [depth for depth in lines]
+        array_of_values = list(lines)
+        return array_of_values
+
+
+def input_text_to_array_ignore_empty_lines(filepath: str):
+    with open(filepath, 'r', encoding='utf-8') as text:
+        lines = text.read().splitlines()
+        array_of_values = [line for line in lines if line != '']
         return array_of_values
